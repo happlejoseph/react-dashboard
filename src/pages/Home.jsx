994180d5e3817e59385001ebc99451f373be02c3
happle joseph <happlejoseph66@gmail.com>
2026-07-14 23:43:25 +0530

    change the design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,16 @@ const Home = ()=> {
 
     return(
         <div className="home-title">
-            <h1>Welcome...</h1>
+            <div className="home-card">
+            <h1>Welcome Back!</h1>
+
+            <p>
+            Welcome to your dashboard. Manage your users and products with a
+            clean, simple, and intuitive interface. Use the navigation menu on
+            the left to get started.
+        </p>
+        </div>
+
         </div>
     )
 }
